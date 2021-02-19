ea2ae8c6c5a3120a96cd923704425c8e247266cb
singo-i <i.singo@gmail.com>
2021-02-19 18:52:18 +0900

    Add main ino file

--- switch-motor-operation-with-arduino-and-keypad/switch-motor-operation-with-arduino-and-keypad.ino
+++ switch-motor-operation-with-arduino-and-keypad/switch-motor-operation-with-arduino-and-keypad.ino
@@ -1,3 +1,34 @@
+#include <Key.h>
+#include <Keypad.h>
+
+#include "Waves.h"
+
+const int out1 = 9;
+
+const byte ROWS = 4;
+const byte COLS = 3;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+byte rowPins[ROWS] = {8, 7, 6, 5};
+byte colPins[COLS] = {4, 3, 2};
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+/*
+ * Current millisecond in Interval
+ * interval: millisecond
+ */
+int current(int interval) {
+  return millis() % interval;
+}
+
+
 void setup() {
   // put your setup code here, to run once:
 
